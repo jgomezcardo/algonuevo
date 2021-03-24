52c9458f8deb623534d2474e13f0cc24f6d53fe1
Ceinmark <Ceinmark@DESKTOP-J0PN6TT>
2021-03-24 09:49:17 +0100

    rama 1 mod

--- src/classs_pene.java
+++ src/classs_pene.java
@@ -4,5 +4,7 @@ public class classs_pene {
 public static void main (String args[]) {
 	System.out.println("hola k ase");
 	System.out.println("hola");
+	
+	System.out.println("rama 1 al power");
 }
 }
